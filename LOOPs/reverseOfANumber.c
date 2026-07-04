51479f9db83e20a6d5af683bf099b62209ff0209
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-07-04 14:12:44 +0530

    minor changes
    code is now fixed

--- LOOPs/reverseOfANumber.c
+++ LOOPs/reverseOfANumber.c
@@ -2,9 +2,10 @@
 int main (){
 int n,lastdigit,rev=0;
 int p;
-int m=n;
+
 printf("Enter A Number: ");
 scanf("%d",&n);
+int m=n;
 while(n!=0){
 
 lastdigit=n%10;
@@ -20,4 +21,6 @@ printf("The Reversed number is %d",rev);
 p= m + rev;
 printf("\nthe sum of the number and its reverse is %d",p);
   return 0;
+  printf("Thank you");
 }
+
