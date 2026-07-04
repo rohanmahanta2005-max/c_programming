5245dbacfcf2de289f4e761cc76e5899b83bdb97
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-07-04 13:57:56 +0530

    code fix
    code is now fixed

--- LOOPs/Fibonacci.c
+++ LOOPs/Fibonacci.c
@@ -5,15 +5,16 @@ printf("Enter a Number : ");
 scanf("%d",&n);
 int a = 0;
 int b = 1;
-int sum = 0;
+int sum ;
+
 for(int i=1;i<=n;i++)
 {
-  printf(" %d", a); 
-   sum = a + b;
-
+     printf(" %d", a);
+    sum = a + b;
+   
    a = b;               //0 1 1 2 3 5 8 13 21 34 55 89 ..........upto nth (fibonacci series)
    b = sum;  
- 
+  
  
   // every term will be the sum of two previous terms
 }
